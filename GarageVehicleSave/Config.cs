69e219e82df08d936162e7f211efc0957623e579
craftycram <github@craftycram.net>
2020-08-22 21:54:43 +0200

    added config & onLoadSpawn

---
+++ GarageVehicleSave/Config.cs
@@ -0,0 +1,19 @@
+﻿using Rage;
+
+namespace GarageVehicleSave
+{
+    internal static class Config
+    {
+        public static readonly InitializationFile INIFile = new InitializationFile(@"Plugins\GarageVehicleSave.ini");
+
+        public static readonly bool SpawnVehicleLeftOnLoad = INIFile.ReadBoolean("General", "SpawnVehicleLeftOnLoad", false);
+        public static readonly bool SpawnVehicleRightOnLoad = INIFile.ReadBoolean("General", "SpawnVehicleRightOnLoad", false);
+
+        public static readonly string SpawnVehicleNameLeft = INIFile.ReadString("General", "SpawnVehicleNameLeft", "");
+        public static readonly string SpawnVehicleNameRight = INIFile.ReadString("General", "SpawnVehicleNameRight", "");
+
+        public static readonly bool SpawnVehicleParkingLotOnLoad = INIFile.ReadBoolean("General", "SpawnVehicleParkingLotOnLoad", false);
+        public static readonly string SpawnVehicleNameParkingLot = INIFile.ReadString("General", "SpawnVehicleNameParkingLot", "");
+
+    }
+}
--- GarageVehicleSave/EntryPoint.cs
+++ GarageVehicleSave/EntryPoint.cs
@@ -1,6 +1,5 @@
 ﻿using Rage;
 using System;
-using System.Runtime.InteropServices;
 
 [assembly: Rage.Attributes.Plugin("GarageVehiclePlugin", Description = "Saves the vehicle you leave in the Mission Row police station.", Author = "craftycram")]
 namespace GarageVehicleSave
@@ -11,19 +10,37 @@ namespace GarageVehicleSave
         {
             GameFiber.StartNew(delegate
             {
-                Game.DisplayHelp("Hello World!");
-                Game.DisplayNotification("This is a notification");
+                //Game.DisplayHelp("Hello World!");
+
+                Game.DisplayNotification("GarageVehiclePlugin loaded successfully");
                 Game.LogTrivial("GarageVehiclePlugin loaded successfully");
+                
                 Vector3 garageRight = new Vector3(462.6767f, -1014.554f, 28.0658f);
                 Vector3 garageMiddle = new Vector3(463.0877f, -1016.823f, 28.08117f);
                 Vector3 garageLeft = new Vector3(463.2594f, -1019.51f, 28.10522f);
+                
+                Vector3 parkingLot = new Vector3(446.0237f, -1025.801f, 27.95778f);
+                // float parkingLotHeading = 5.635557f;
+
                 Vehicle savedVehicleLeft = null;
                 Vehicle savedVehicleRight = null;
+
+                if(Config.SpawnVehicleLeftOnLoad)
+                {
+                    savedVehicleLeft = new Vehicle(Config.SpawnVehicleNameLeft, garageLeft, 90f);
+                }
+                if (Config.SpawnVehicleRightOnLoad)
+                {
+                    savedVehicleRight = new Vehicle(Config.SpawnVehicleNameRight, garageRight, 90f);
+                }
+                if (Config.SpawnVehicleParkingLotOnLoad)
+                {
+                    savedVehicleRight = new Vehicle(Config.SpawnVehicleNameParkingLot, parkingLot, 0f);
+                }
 
                 while (true)
                 {
                     GameFiber.Yield();
-
                     //while (Game.LocalPlayer.Character.CurrentVehicle != null)
                     //{
                     //GameFiber.Yield();
@@ -87,24 +104,16 @@ namespace GarageVehicleSave
                                 savedVehicleRight.Wash();
                             }
                         }
-
                     }
-
 
                     if (Game.IsKeyDown(System.Windows.Forms.Keys.Enter))
                     {
-                        Game.DisplayNotification(Convert.ToString(Game.LocalPlayer.Character.Position.X) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Y) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Z));
-                        Game.LogTrivial(Convert.ToString(Game.LocalPlayer.Character.Position.X) + "f, " + Convert.ToString(Game.LocalPlayer.Character.Position.Y) + "f, " + Convert.ToString(Game.LocalPlayer.Character.Position.Z) + "f");
+                        Game.DisplayNotification(Convert.ToString(Game.LocalPlayer.Character.Position.X) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Y) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Z) + ", " + Convert.ToString(Game.LocalPlayer.Character.Heading));
+                        Game.LogTrivial(Convert.ToString(Game.LocalPlayer.Character.Position.X) + "f, " + Convert.ToString(Game.LocalPlayer.Character.Position.Y) + "f, " + Convert.ToString(Game.LocalPlayer.Character.Position.Z) + "f" + ", " + Convert.ToString(Game.LocalPlayer.Character.Heading) + "f");
                     }
                     //}
 
-
-
                 }
-
-
-
-
             });
         }
     }
