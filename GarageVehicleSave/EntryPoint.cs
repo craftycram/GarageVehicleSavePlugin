30a9d0dcc09a1086fe522a85a04bb10f2dbb6038
craftycram <github@craftycram.net>
2020-08-22 06:32:04 +0200

    fixed vehicle saving & repairing

--- GarageVehicleSave/EntryPoint.cs
+++ GarageVehicleSave/EntryPoint.cs
@@ -1,5 +1,6 @@
 ﻿using Rage;
 using System;
+using System.Runtime.InteropServices;
 
 [assembly: Rage.Attributes.Plugin("GarageVehiclePlugin", Description = "Saves the vehicle you leave in the Mission Row police station.", Author = "craftycram")]
 namespace GarageVehicleSave
@@ -13,6 +14,11 @@ namespace GarageVehicleSave
                 Game.DisplayHelp("Hello World!");
                 Game.DisplayNotification("This is a notification");
                 Game.LogTrivial("GarageVehiclePlugin loaded successfully");
+                Vector3 garageRight = new Vector3(462.6767f, -1014.554f, 28.0658f);
+                Vector3 garageMiddle = new Vector3(463.0877f, -1016.823f, 28.08117f);
+                Vector3 garageLeft = new Vector3(463.2594f, -1019.51f, 28.10522f);
+                Vehicle savedVehicleLeft = null;
+                Vehicle savedVehicleRight = null;
 
                 while (true)
                 {
@@ -21,18 +27,74 @@ namespace GarageVehicleSave
                     //while (Game.LocalPlayer.Character.CurrentVehicle != null)
                     //{
                     //GameFiber.Yield();
-                    Vector3 garageLeft = new Vector3(469.007f, -1013.146f, 0f);
-                    Vector3 garageRight = new Vector3();
-                    if (Game.LocalPlayer.Character.DistanceTo(garageLeft) < 30f)
+
+                    // Vehicle LEFT
+                    if (Game.LocalPlayer.Character.DistanceTo(garageLeft) < 2f)
                     {
-                        Game.DisplayNotification("You're nearby a Garage!");
+                        if (Game.LocalPlayer.Character.CurrentVehicle != null)
+                        {
+                            Game.DisplayNotification("Vehicle saved to left garage!");
+                            savedVehicleLeft = Game.LocalPlayer.Character.CurrentVehicle;
+                            savedVehicleLeft.MakePersistent();
+                        }
                         //break;
                     }
 
-                    if (Game.IsKeyDown(System.Windows.Forms.Keys.P))
+                    if(savedVehicleLeft != null)
+                    {
+                        if (savedVehicleLeft.DistanceTo(garageLeft) < 2f)
+                        {
+                            savedVehicleLeft.MakePersistent();
+                        }
+                    }
+
+                    // Vehicle Right
+                    if (Game.LocalPlayer.Character.DistanceTo(garageRight) < 2f)
+                    {
+                        if (Game.LocalPlayer.Character.CurrentVehicle != null)
+                        {
+                            Game.DisplayNotification("Vehicle saved to right garage!");
+                            savedVehicleRight = Game.LocalPlayer.Character.CurrentVehicle;
+                            savedVehicleRight.MakePersistent();
+                        }
+                        //break;
+                    }
+
+                    if (savedVehicleRight != null)
+                    {
+                        if (savedVehicleRight.DistanceTo(garageRight) < 2f)
+                        {
+                            savedVehicleRight.MakePersistent();
+                        }
+                    }
+
+                    // Clean vehicles
+                    if (Game.LocalPlayer.Character.DistanceTo(garageMiddle) > 20f && Game.LocalPlayer.Character.DistanceTo(garageMiddle) < 50f)
+                    {
+                        if (savedVehicleLeft != null && savedVehicleLeft.DistanceTo(garageLeft) < 2f)
+                        {
+                            if (Game.LocalPlayer.Character.CurrentVehicle != savedVehicleLeft)
+                            {
+                                savedVehicleLeft.Repair();
+                                savedVehicleLeft.Wash();
+                            }
+                        }
+                        if (savedVehicleRight != null && savedVehicleRight.DistanceTo(garageRight) < 2f)
+                        {
+                            if (Game.LocalPlayer.Character.CurrentVehicle != savedVehicleRight)
+                            {
+                                savedVehicleRight.Repair();
+                                savedVehicleRight.Wash();
+                            }
+                        }
+
+                    }
+
+
+                    if (Game.IsKeyDown(System.Windows.Forms.Keys.Enter))
                     {
                         Game.DisplayNotification(Convert.ToString(Game.LocalPlayer.Character.Position.X) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Y) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Z));
-                        Game.LogTrivial(Convert.ToString(Game.LocalPlayer.Character.Position.X) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Y) + ", " + Convert.ToString(Game.LocalPlayer.Character.Position.Z));
+                        Game.LogTrivial(Convert.ToString(Game.LocalPlayer.Character.Position.X) + "f, " + Convert.ToString(Game.LocalPlayer.Character.Position.Y) + "f, " + Convert.ToString(Game.LocalPlayer.Character.Position.Z) + "f");
                     }
                     //}
 
